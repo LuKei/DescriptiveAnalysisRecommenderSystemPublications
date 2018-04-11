e1cc5e643e08f6efe69bfefb02eacb4cd9a180aa
lukas <lukas.keilholz@web.de>
2018-04-11 15:31:02 +0200

    Initial commit

--- test.py
+++
@@ -1,6 +0,0 @@
-from fuzzywuzzy import fuzz
-
-def match_author(author_a, author_b, threshold=0.95):
-    return True if fuzz.token_sort_ratio(author_a, author_b) > threshold else False
-
-print(match_author('James Caveree', 'Negar Haririr'))
