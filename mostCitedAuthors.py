08d23e7e303b4901e3cb789c29b4e7e0e3813541
lukas <lukas.keilholz@web.de>
2018-04-14 13:59:03 +0200

    Initial commit

--- mostCitedAuthors.py
+++ mostCitedAuthors.py
@@ -1,9 +1,10 @@
+import pickle
 import pandas
+import sqlite3
 import time
 import Levenshtein
 import traceback
-import pickle
-from datetime import datetime
+
 
 def match_title(title_a, title_b, author_match=False, threshold=(0.90, 0.80)):
     ratio = Levenshtein.ratio(title_a, title_b)
@@ -16,7 +17,7 @@ def match_authors(authors_a, authors_b, threshold=0.85):
         authors_a, authors_b) > threshold else False
 
 
-def generate_reference_matches(reference_df):
+def generate_reference_matches(reference_df, publication_df):
     '''
     Iterates of the reference dataframe to resolve duplicate papers and find citation intersections
     :param reference_df:
@@ -24,9 +25,9 @@ def generate_reference_matches(reference_df):
     '''
     c, match = 0, []
     reference_df['ref_id'] = reference_df.index
-    for reference in reference_df.itertuples():
+    for publication in publication_df.itertuples():
         ind_series = reference_df.apply(lambda x: match_title(
-            x['title'], reference.title, match_authors(x['authors'], reference.authors)), axis=1)
+            x['title'], publication.title, match_authors(x['authors'], publication.authors)), axis=1)
         try:
             indices = reference_df[ind_series].ref_id.as_matrix()
         except ValueError as err:
@@ -35,19 +36,23 @@ def generate_reference_matches(reference_df):
             c += 1
             continue
         if indices.size:
-            match.append(set(indices))
+            match.append({publication.id: set(indices)})
             reference_df.drop(indices, inplace=True)
+        else:
+            match.append({publication.id: {-1}})
         c += 1
         if c % 100 == 0:
             print(time.strftime("%H:%M:%S", time.gmtime()) + " #" + str(c))
     return match
 
 
-df = pandas.read_pickle('pickles/citationHierarchy.pkl')
+reference_df = pandas.read_pickle('pickles/citationHierarchy.pkl')
+connIn = sqlite3.connect('publications20180322_184739 _erweitert.db')
+publication_df = pandas.read_sql('select * from publication', connIn)
 
-matches = generate_reference_matches(df)
+matches = generate_reference_matches(reference_df, publication_df)
 
-with open('pickles/matchedCitationHierarchy.pkl', 'wb') as f:
+with open('pickles/matchedCitationHierarchy2.pkl', 'wb') as f:
     pickle.dump(matches, f, pickle.HIGHEST_PROTOCOL)
 
 print('success')
